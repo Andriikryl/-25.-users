e87ee92ab5eaa5b98233d71366645da050b45796
Amdrii <andreyk557@gmail.com>
2023-01-04 22:55:18 +0200

    add

--- public/app.js
+++ public/app.js
@@ -1,16 +1,51 @@
+const { response } = require("express");
+
 const API_URL = "http://127.0.0.1:3000"
 
+const usersBlock = document.querySelector(".usersBlock");
+
+const addUsersHtml = ({id, name, age}) =>{
+    const div = document.createElement("div")
+    div.setAttribute("data-id", id);
+    
+    div.insertAdjacentHTML(
+        "beforeend",
+        `<p><span>user: ${name}; age: ${age}</span><button>edit</button></p>`
+        );
+
+        usersBlock.append(div)
+}
+
+
+const getUsers = () => 
 fetch(`${API_URL}/users`)
 .then((response) => response.json())
 .then((data) => {
-    const usersBlock = document.querySelector(".usersBlock");
-    
-    data.forEach(({id, name, age}) => {
-        const div = document.createElement("div")
-        div.setAttribute('data-id', id);
-        
-        
-    });
-}).catch((error) =>{
+    data.forEach(addUsersHtml)
+})
+.catch((error) =>{
     alert(error.message)
 });
+
+document.forms.user.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const name = this.name.value;
+    const age = this.age.value;
+
+    fetch(`${API_URL}/users`, {
+        method: "POST",
+        body: JSON.stringify({name, age}),
+        headers: {
+            "Content-Type": "application/json",
+
+        },
+    })
+    .then((response) => response.json())
+    .then((data)=>{
+
+    });
+});
+
+
+
--- server.js
+++ server.js
@@ -13,6 +13,19 @@ app.get("/users", (request, response) => {
     return response.send(JSON.stringify(usersData));
 });
 
+app.post("/users", (raq, res) => {
+    const id = usersData.length === 0 ? 1 : usersData.at(-1).id + 1;
+
+    console.log(req.body);
+
+    usersData.push({
+        id,
+        ...req.body,
+    });
+
+    
+});
+
 app.listen(3000, () => {
     console.log("Server started at port: 3000" )
 });
